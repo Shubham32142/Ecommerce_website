a3daf23a9b016dfed6ec54aead8f25f1e9c2f959
Shubham32142 <shubhamdubey23042004@gmail.com>
2024-10-16 20:56:37 +0530

    Did some changes

--- nodeJS/Routes/products.routes.js
+++ nodeJS/Routes/products.routes.js
@@ -16,10 +16,10 @@ import {
 export function routes(server) {
   server.post("/api/products", createProduct);
   server.get("/api/allProducts", fetchProducts);
-  server.get("/api/:id", productById);
+  server.get("/api/:id", authenticationToken, productById);
   server.get("/cart/allItems", authenticationToken, getCartItems);
-  server.put("/api/:id", updateProduct);
-  server.delete("/api/:id", deleteProduct);
+  server.put("/api/:id", authenticationToken, updateProduct);
+  server.delete("/api/:id", authenticationToken, deleteProduct);
   server.post("/cart", authenticationToken, cartItem);
   server.put("/cart/:id", authenticationToken, updateCart);
   server.delete("/cart/:id", deleteCart);
